6f9cdb2dc6f63a97542a33a44aa96a1e8f3f6a17
Kfir Nissim <kfirr99.gmail.com>
2020-01-11 15:13:56 +0200

    set the song card layout

--- desktop/src/app/songs-menu/song-card/song-card.component.ts
+++ desktop/src/app/songs-menu/song-card/song-card.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from "@angular/core";
 
+enum Drawers {
+  SHARE,
+  DOWNLOAD
+}
+
 @Component({
   selector: "app-song-card",
   templateUrl: "./song-card.component.html",
@@ -8,7 +13,18 @@ import { Component, OnInit, Input } from "@angular/core";
 export class SongCardComponent implements OnInit {
   @Input() song: { title: string; img: string };
 
+  openedDrawer = null;
+  DRAWERS = Drawers;
+
   constructor() {}
 
   ngOnInit() {}
+
+  /**
+   * Open a drawer, if it already open, close it
+   * @param drawer the enum of the drawer.
+   */
+  toggleDrawer(drawer: Drawers) {
+    this.openedDrawer = this.openedDrawer !== drawer ? drawer : null;
+  }
 }
